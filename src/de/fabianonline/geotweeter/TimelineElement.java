6ecfaa2456da4f1a2eb2cdb067dbbed935c2f7fa
Fabian Schlenz <mail@fabianonline.de>
2012-09-24 13:49:40 +0200

    Keine Ahnung, warum wir zwei TimelineElements hatten - dieses ist aber auf jeden Fall unwichtig und kann weg.

--- src/de/fabianonline/geotweeter/TimelineElement.java
+++
@@ -1,9 +0,0 @@
-package de.fabianonline.geotweeter;
-
-
-public abstract class TimelineElement {
-	
-	abstract public String getTextForDisplay();
-	abstract public CharSequence getSourceText();
-	abstract public String getAvatarSource();
-}
