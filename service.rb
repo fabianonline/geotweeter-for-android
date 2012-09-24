120ee262052c47665ff5fb84b2013aa65e72769f
Fabian Schlenz <mail@fabianonline.de>
2012-09-24 11:46:34 +0200

    Es kann mehrere reg_ids pro Token geben und die Threads sollten auskunftsfreudiger sein.

--- service.rb
+++ service.rb
@@ -5,6 +5,7 @@ require 'bundler'
 require 'digest/sha2'
 Bundler.require
 
+Thread.abort_on_exception = true
 $settings = YAML::load_file("settings.yml") rescue {}
 $digest = Digest::SHA256.new
 
@@ -14,55 +15,80 @@ def update()
 	lines = IO.readlines("command.txt")
 	lines.each do |line|
 		command, token, secret, reg_id = *line.split(" ")
-		hash_value = $digest.hexdigest([token, secret, reg_id].join).slice(0, 7)
+		hash_value = $digest.hexdigest([token, secret].join).slice(0, 7)
 		if command=="add"
-			next if $settings.has_key? hash_value
 			next unless reg_id && reg_id.length>5
-			hash = {:token=>token, :secret=>secret, :reg_id=>reg_id}
-			clone = hash.clone
-			t = Thread.new {
-				stream(clone, hash_value)
-			}
-			hash[:thread] = t
-			$settings[hash_value] = hash
+			if $settings.has_key? hash_value
+				next if $settings[hash_value].has_key?(:reg_ids) && $settings[hash_value][:reg_ids].include?(reg_id)
+				$settings[hash_value][:reg_ids] << reg_id
+				$settings[hash_value][:thread].kill if $settings[hash_value][:thread]
+				log("Adding new reg_id to #{hash_value}")
+			else
+				hash = {:token=>token, :secret=>secret, :reg_ids=>[reg_id]}
+				$settings[hash_value] = hash
+				log("Adding stream for #{hash_value}")
+			end
+			start_thread(hash_value)
 		elsif command=="del"
 			puts "Del. #{hash_value}"
 			next unless $settings.has_key? hash_value
-			puts "Killing Thread."
-			$settings[hash_value][:thread].kill
-			$settings.delete hash_value
-			p $settings
+			$settings[hash_value][:thread].kill if $settings[hash_value][:thread]
+			$settings[hash_value][:reg_ids].delete reg_id
+			if $settings[hash_value][:reg_ids].empty?
+				$settings.delete hash_value
+				log("Stream for #{hash_value} has no more reg_ids left.")
+			else
+				log("Removed reg_id from #{hash_value}")
+				start_thread(hash_value)
+			end
 		end
 	end
 	save_settings
 end
 
 def save_settings
-	data = $settings.clone
-	new_hash = {}
-	data.each {|key, hash| hash.delete(:thread); new_hash[key] = hash}
-	File.open(File.join(File.dirname(__FILE__), "settings.yml"), "w") {|f| f.write YAML.dump(new_hash)}
+	clone = $settings.clone
+	copy = {}
+	clone.each do |key, hash|
+		hash.delete :thread
+		copy[key] = hash
+	end
+	File.open(File.join(File.dirname(__FILE__), "settings.yml"), "w") {|f| f.write YAML.dump(copy)}
 end
 
-def stream(data, hash_value)
-	puts "Ich bin ein neuer Stream!"
-	result = $gcm_sender.send([data[:reg_id]], {:data=>{:msg=>"Hallo Welt"}})
-	puts "completed."
+def stream(hash)
+	data = $settings[hash]
+	log hash, "New thread is running. Settings: " + data.inspect
+	#result = $gcm_sender.send(data[:reg_ids], {:data=>{:msg=>"Hallo Welt"}})
+	while true
+		log hash, "ich lebe noch."
+		sleep (10)
+	end
 	p result
 end
 
-$settings.each do |key, hash|
-	clone = hash.clone
-	t = Thread.new { stream(clone, key) }
-	hash[:thread] = t
+def start_thread(hash)
+	t = Thread.new do 
+		stream(hash)
+	end
+	$settings[hash][:thread] = t
 end
 
-p $settings
+def log(hash, string=nil)
+	hash, string = "", hash unless string
+	puts "%-10s %s" % [hash, string]
+end
+
+$settings.each do |key, hash|
+	start_thread(key)
+end
+
 
 Listen.to(File.dirname(__FILE__), :filter=>/^command\.txt$/) do |modified, added, removed|
+	log "New command found."
 	if removed.empty?
 		update()
-		FileUtils.rm(File.join(File.dirname(__FILE__), "command.txt"))
+		#FileUtils.rm(File.join(File.dirname(__FILE__), "command.txt"))
 	end
 end
 
