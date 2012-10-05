112e356b4cf642ff5c9e2a71906d5f9192932643
el-emka <lutz.krumme@tu-dortmund.de>
2012-10-06 00:06:19 +0200

    Tweet-Objekte für Konversationsverfolgung vorbereitet.

--- src/de/fabianonline/geotweeter/timelineelements/Tweet.java
+++ src/de/fabianonline/geotweeter/timelineelements/Tweet.java
@@ -20,6 +20,11 @@ public class Tweet extends TimelineElement {
 	public View view;
 	public String source;
 	public Entities entities;
+	public long in_reply_to_status_id;
+	public String in_reply_to_status_id_str;
+	public long in_reply_to_user_id;
+	public String in_reply_to_user_id_str;
+	
 	
 	public long getID() {
 		return id;
