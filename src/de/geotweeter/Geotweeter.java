f384b4ed08608261d44846e8f9920a3be9c720f6
Fabian Schlenz <mail@fabianonline.de>
2012-10-11 23:59:01 +0200

    ACRA bittet jetzt um Erlaubnis, bevor ein Fehlerbericht gesendet wird.

--- src/de/geotweeter/Geotweeter.java
+++ src/de/geotweeter/Geotweeter.java
@@ -4,7 +4,12 @@ import android.app.Application;
 import org.acra.*;
 import org.acra.annotation.*;
 
-@ReportsCrashes(formKey="dHZaTjh2ZHcyd3dScFRTUFUzVmxlaUE6MQ")
+@ReportsCrashes(formKey                = "dHZaTjh2ZHcyd3dScFRTUFUzVmxlaUE6MQ",
+                mode                   = ReportingInteractionMode.DIALOG,
+                resToastText           = R.string.crash_toast_text,
+                resDialogText          = R.string.crash_dialog_text,
+                resDialogCommentPrompt = R.string.crash_dialog_comment_prompt,
+                resDialogOkToast       = R.string.crash_dialog_ok_toast)
 public class Geotweeter extends Application {
 	@Override
 	public void onCreate() {
