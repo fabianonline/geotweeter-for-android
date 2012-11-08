a05709b05eb43168cc0f87dbc3f43a2268c94ff1
el-emka <lutz.krumme@tu-dortmund.de>
2012-11-08 15:18:31 +0100

    YFrog-Bugfix

--- src/de/geotweeter/apiconn/YfrogApiAccess.java
+++ src/de/geotweeter/apiconn/YfrogApiAccess.java
@@ -10,8 +10,8 @@ public class YfrogApiAccess {
 
 	public static Pair<URL, URL> getUrlPair(Url url) throws MalformedURLException {
 		URL screen_url = new URL(url.expanded_url);
-		return new Pair<URL, URL>(new URL("http://" + screen_url.getHost() + "/" + screen_url.getPath() + ":small"), 
-				new URL("http://" + screen_url.getHost() + "/" + screen_url.getPath() + ":medium"));
+		return new Pair<URL, URL>(new URL("http://" + screen_url.getHost() + screen_url.getPath() + ":small"), 
+				new URL("http://" + screen_url.getHost() + screen_url.getPath() + ":medium"));
 	}
 
 }
