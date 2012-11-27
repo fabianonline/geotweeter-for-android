1c9f4aebf718b63fe6cc3f9019daf86faaaa9a01
el-emka <lutz.krumme@tu-dortmund.de>
2012-11-27 17:26:22 +0100

    Der Comparator arbeitete inkonsistent bei Tweets mit gleichem Timestamp

--- src/de/geotweeter/timelineelements/TLEComparator.java
+++ src/de/geotweeter/timelineelements/TLEComparator.java
@@ -6,7 +6,8 @@ public class TLEComparator implements Comparator<TimelineElement> {
 
 	@Override
 	public int compare(TimelineElement lhs, TimelineElement rhs) {
-		return ((lhs.created_at.getTime() > rhs.created_at.getTime()) ? -1 : 1);
+		return (lhs.created_at.getTime() == rhs.created_at.getTime() ? 0 : 
+			(lhs.created_at.getTime() > rhs.created_at.getTime()) ? -1 : 1);
 	}
 
 }
