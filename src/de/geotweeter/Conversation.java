7ac851825001b4eb8108f43edb1e7671394e0051
el-emka <lutz.krumme@tu-dortmund.de>
2013-01-31 00:38:18 +0100

    Jetzt noch heiler!

--- src/de/geotweeter/Conversation.java
+++ src/de/geotweeter/Conversation.java
@@ -76,8 +76,8 @@ public class Conversation {
 				return null;
 			}
 			Tweet current = (Tweet) current_element;
-			while (current.inReplyToStatusId != 0) {
-				long predecessor_id = current.inReplyToStatusId;
+			while (current.in_reply_to_status_id != 0) {
+				long predecessor_id = current.in_reply_to_status_id;
 				try {
 					current = (Tweet) TimelineActivity.availableTweets.get(predecessor_id);
 				} catch (NullPointerException e) {
